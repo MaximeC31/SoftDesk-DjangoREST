6f563133d82eaadf45d835360bfddee33d08e257
Maxime CHAMPIGNEUX <contact@maximechampigneux.fr>
2026-06-24 23:40:51 +0200

    fix: improve registration validation errors

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,20 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (  # type: ignore[assignment, operator]
+        (
+            "RGPD",
+            {"fields": ("age", "can_be_contacted", "can_data_be_shared")},
+        ),
+    )
+
+    list_display = UserAdmin.list_display + (  # type: ignore[operator]
+        "age",
+        "can_be_contacted",
+        "can_data_be_shared",
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
--- users/serializers.py
+++ users/serializers.py
@@ -1,39 +1,67 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
+from rest_framework.validators import UniqueValidator
+
+User = get_user_model()
 
 
-class UserSerializer(serializers.Serializer):
-    username = serializers.CharField()
+class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message="This username is already taken.",
+            ),
+        ],
+    )
     password = serializers.CharField(write_only=True)
-    age = serializers.IntegerField()
     can_be_contacted = serializers.BooleanField(required=True)
     can_data_be_shared = serializers.BooleanField(required=True)
 
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "password",
+            "age",
+            "can_be_contacted",
+            "can_data_be_shared",
+        ]
+        read_only_fields = ["id"]
+
+    def validate_password(self, password):
+        validate_password(password)
+        return password
+
     def validate(self, attrs):
-        if "username" not in attrs:
-            raise ValidationError("Username is required.")
-        if "password" not in attrs:
-            raise ValidationError("Password is required.")
-        if "age" not in attrs:
-            raise ValidationError("Age is required.")
-        if "can_be_contacted" not in attrs:
-            raise ValidationError("Consent to be contacted is required.")
-        if "can_data_be_shared" not in attrs:
-            raise ValidationError("Consent to share data is required.")
-
-        age_input = self.initial_data["age"]
-
-        if type(age_input) is not int:
-            raise ValidationError({"age": "Age must be an integer."})
-
-        if age_input < 15:
-            raise ValidationError("Age must be 15 or older.")
+        if type(self.initial_data.get("age")) is not int:
+            raise serializers.ValidationError(
+                {
+                    "age": "Age must be an integer.",
+                }
+            )
+        if type(self.initial_data.get("can_be_contacted")) is not bool:
+            raise serializers.ValidationError(
+                {
+                    "can_be_contacted": (
+                        "Consent to be contacted must be a boolean value."
+                    ),
+                }
+            )
+        if type(self.initial_data.get("can_data_be_shared")) is not bool:
+            raise serializers.ValidationError(
+                {
+                    "can_data_be_shared": (
+                        "Consent to share data must be a boolean value."
+                    ),
+                }
+            )
 
         return attrs
 
     def create(self, validated_data):
-        User = get_user_model()
         password = validated_data.pop("password")
         user = User(**validated_data)
         user.set_password(password)
